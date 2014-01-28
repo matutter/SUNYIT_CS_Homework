cfcaf0332d1f40cc851edeff58e7f192759a7873
mat <mat@mobile.(none)>
2014-01-28 16:05:52 -0500

    updates

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="icon" href="../images/favicon.ico">
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css">
+
 <link rel="stylesheet" href="../css/bootstrap.min.css"/>
 <link rel="stylesheet" href="../css/main.css"/>
 <!--[if 1t IE 7]>
@@ -19,30 +19,27 @@
 <title>utterm@sunyit.edu</title>
 </head>
 <body>
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="navbar-brand"> <img src="../images/utterm.png"> </div>
+</nav>
 
- <nav class="navbar navbar-inverse" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><img src="../images/utterm.png"></a>
-        </div>
-	        <div class="navbar-collapse collapse">
-	          <ul class="nav nav-tabs">
-				<li class="active" id="cs249"><a href="#">CS249</a></li>
-				<li id="cs350"><a href="#">CS350</a></li>
-				<li id="cs350"><a href="#">CS350</a></li>
-				<li id="other"><a href="#">Other</a></li>
-			</ul>
-	        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
+<div class="col-sm-2 nav-side">
+	<ul class="nav nav-pills nav-stacked">
+		<li class="active" id="cs249"><a href="#">CS249</a></li>
+		<li id="cs350"><a href="#">CS351</a></li>
+		<li id="cs350"><a href="#">CS445</a></li>
+		<li id="other"><a href="#">Other</a></li>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br><br><br><br><br><br><br><br><br><br>
+	</ul>
+</div>
 
-<div class="col-sm-10 col-sm-offset-1">
+<div class="col-sm-offset-1 col-sm-8">
 	<div class="panel panel-default ">
 	  <div class="panel-heading"><h3>Homework</h3></div>
 	  <div class="panel-body">
@@ -51,21 +48,19 @@
 	</div>
 </div>
 
-<div class="well well-lg">...</div>
+<!--<div class="well well-lg">...</div> -->
 
 </body>
 <script>
 $(document).ready(function(){
 	loadPage("cs249")
-	$('nav li').click(function(){
-		if($(this).index() == 0) return;
+	$('.nav li').click(function(){
 		$('.panel-body').empty()
 		$(this).addClass('active').siblings().removeClass('active')
-		$('.collapse').removeClass('in')
-		$('#collapse'+$(this).index()).addClass('in')
 		loadPage($(this).attr('id'))
 	})
 })
 
 </script>
+<!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css">-->
 </html>
