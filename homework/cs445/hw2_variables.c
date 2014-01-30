0b7dd44b30418201ab29e1a644e1029ba2bd8acd
mat <mat@mat-vm.(none)>
2014-01-30 02:16:53 -0500

    finished hw

--- homework/cs445/hw2_variables.c
+++ homework/cs445/hw2_variables.c
@@ -1,37 +1,67 @@
-//hm1 
+//hw1 
 #include <stdio.h>
 #include <sys/types.h>
-#include <wchar.h>
-int fwide(FILE *fp, int mode);
-int fflush(FILE *fp);
-
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+char fname[255]; 
+struct stat st;
+void noRace();
+static void handler(int signo);
 
 int main(int argc, char const *argv[])
 {
-	
-	printf(	"Write a program at reads the names of files by reading the standard input.\n"
-			"Each time it reads a filename, it creates a new process (using fork) that \n"
-			"determines the file's length, using the \"stat\" system call. \n"
-			"If the file length is odd, the child process uses the \"kill\" system call \n"
-			"to send the SIGUSR1 signal to the parent. If the file length is even, \n"
-			"the child process sends the SIGUSR2 signal to the parent. When the parent \n"
-			"receives the signal, it prints the name of the file and whether its length\n"
-			" was odd or even. After printing, the process reads the next line of input \n"
-			"from standard input and continues.\n\n\n");
+	int i=0, n=0, k=0, status;
+	FILE *fp;
+	pid_t pid=1;
 
-	int pid = fork();
+	while(1) {
 
-	if (pid < 0) { // error occurred 
-		fprintf(stderr, "Fork failed!\n");
-		//exit(-1);
-	}else if (pid == 0) { // child process 
-		printf("I am the child, return from fork=%d\n", pid);
-		//execlp("/bin/ps", "ps", NULL);
-	}else { // parent process 
-		printf("I am the parent, return from fork, child pid=%d\n", pid);
-		printf("Parent exiting!\n");
-		//exit(0);
+		signal(SIGUSR1, handler);
+		signal(SIGUSR2, handler);
+
+		if(pid == 0)
+		{
+			fp = fopen(fname, "r");
+			if(fp == NULL){
+				printf("Child cannot find [%s].", fname);
+				fclose(fp);
+			}
+			else {
+				stat(fname, &st);
+				fclose(fp);
+				n = st.st_size;
+				pid = getpid();
+				if(n%2) kill(pid, SIGUSR1);
+					else kill(pid, SIGUSR2);
+			}
+
+				kill(getpid(), 9); //send kill
+				wait(&status);	//double tap
+		}
+		else
+		{
+			noRace();		
+
+			printf("Enter filenames until you're happy. 'die' to end.\n");
+			scanf("%s", fname);
+			if(!strcmp(fname,"die")) return 0;
+			pid = fork();
+		}
+
 	}
-
+	printf("Bye bye\n\n");
 	return 0;
+}
+
+static void handler(int signo) {
+	if(signo-10) printf("%s is even.\n", fname); else printf("%s is odd.\n", fname);
+}
+
+void noRace() {
+	int i;
+	for( i=0;i<3;i++ )
+		for( i=0;i<100000000;i++ );
 }
